46c0ca0b4a5a93683d9de89566803840ae4e3f47
Whyryou <ploypimjb77@gmail.com>
2023-12-01 21:57:55 +0700

    commit

--- admin_view.php
+++ admin_view.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>รายละเอียดพนักงาน</title>
-    <<style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>ข้อมูลพนักงาน</title>
+     <style>
         .car-chiangrai{
             background-color:#fff;
             padding: 20px;
@@ -23,17 +25,17 @@
         height: auto;
         margin-top: 10px;
        }
-    </>
-    <link rel="stylesheet" type="text/css" href="css/styles_menubar.css">
-
+    </style>
+    <link rel="stylesheet" type="text/css" href="css/style_menuber.css">
 
 </head>
+
 <body>
     <div class="container my-4">
         <header class="d-flex justify-content-between my-4">
-            <h1>รายละเอียดพนักงาน</h1>
+            <h1>ข้อมูลพนักงาน</h1>
             <div>
-            <a href="admin_user.php" class="btn btn-primary">กลับ</a>
+                <a href="admin_user.php" class="btn btn-primary">กลับ</a>
             </div>
         </header>
         <div class="car-chiangrai p-5 my-4">
@@ -44,22 +46,22 @@
                 $sql = "SELECT * FROM useradmin WHERE id = $id";
                 $result = mysqli_query($conn, $sql);
                 while ($row = mysqli_fetch_array($result)) {
-                 ?>
-                 <h3>ชื่อ-นามสกุล:</h3>
-                 <p><?php echo $row["name"]; ?></p>
-                 <h3>ชื่อผู้ใช้งาน:</h3>
-                 <p><?php echo $row["user_name"]; ?></p>
-                 
-                  
-                 <?php
+                    ?>
+                    <h3>ชื่อ-นามสกุล:</h3>
+                    <p><?php echo $row["name"]; ?></p>
+                    <h3>ชื่อผู้ใช้งาน:</h3>
+                    <p><?php echo $row["user_name"]; ?></p>
+                    
+
+            <?php
                 }
-            }
-            else{
+            } else {
                 echo "<h3>ไม่พบข้อมูลพนักงาน</h3>";
             }
             ?>
-            
+
         </div>
     </div>
 </body>
+
 </html>
--- profile.php
+++ profile.php
@@ -1,3 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>โปรไฟล์</title>
+     <style>
+        .car-chiangrai{
+            background-color:#fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-bottom: 20px;
+        }
+       h3{
+        font-size: 24px;
+        margin-top: 20px;
+       }
+       img {
+        max-width: 100%;
+        height: auto;
+        margin-top: 10px;
+       }
+    </style>
+    <link rel="stylesheet" type="text/css" href="css/style_menuber.css">
+
+</head>
+
+<boby>
+    <div class="container my-4">
+        <header class="d-flex justify-content-between my-4">
+            <h1>โปรไฟล์ของคุณ</h1>
+            <div>
+                <a href="menubar.php" class="btn btn-primary">กลับ</a>
+            </div>
+        </header>
+        <div class="car-chiangrai p-5 my-4">
 <?php
 session_start();
 include "db_conn.php";
@@ -10,7 +51,7 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['name']) && isset($_SESSION
     $id = $_SESSION['id'];
 
     // แสดงข้อมูลโปรไฟล์
-    echo "<h1>โปรไฟล์ของคุณ</h1>";
+   
     echo "<p>ชื่อผู้ใช้งาน: $user_name</p>";
     echo "<p>ชื่อ-นามสกุล: $name</p>";
     echo "<p>อีเมล: $email</p>";
@@ -24,3 +65,7 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['name']) && isset($_SESSION
     exit();
 }
 ?>
+ </div>
+    </div>
+</boby>
+</html>
